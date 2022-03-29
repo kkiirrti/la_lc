db762c9029df9b965bd2db778d7c6ee9c6b12261
kirti <kirti@kritin.in>
2022-03-29 20:20:03 +0530

    minor changes

--- src/simplify.py
+++ src/simplify.py
@@ -26,15 +26,12 @@ def is_simple_sentence(sentence):
     True
     #>>> is_simple_sentence("लेकिन अगर आप उनको ध्यान से देखेंगे तो आप पाएँगे कि इनमें से कुछ की टिमटिमाहट अन्य से अलग है।")
     False
-
-
     """
-    # connective = "कि"
-    # return not is_word_present(connective, sentence)
 
     connectives = ['कि', 'तथा', 'यद्यपि', 'और', 'मानो', 'परन्तु', 'जो', 'जिसे', 'जिसने ', 'जिसको', 'जिसका', 'जिसके', 'जिससे', 'जिसमें', 'जिनको', 'जिनके', 'जिनसे', 'जिनमें', 'जिन्हें', 'जिन्होंने', 'इसलिए', 'इसलिये', 'इसीलिये', 'क्योंकि', 'जैसे', 'किन्तु', 'एवं', 'यद्यपि', 'तथापि', 'भले ही', 'तो', 'अगर', 'मगर', 'अतः', 'चूंकि', 'चूँकि', 'जिस तरह', 'जिस प्रकार', 'लेकिन', 'जब', 'तब', 'तभी', 'या','जहाँ','वरना','अन्यथा','ताकि', 'बशर्तें' ,'जैसे', 'जबकि', 'यदि', 'मानो', 'वरन' , 'परंतु' , 'किंतु' , 'हालाँकि' , 'हालांकि', 'जिस', 'जिन']
+
     for x in connectives:
-        complex_sentence = x in [ws.strip(',.?!') for ws in sentence.split()]
+        complex_sentence = x in [ws.strip(',.?!-।:;') for ws in sentence.split()]
         if complex_sentence: break
     return not complex_sentence
 
